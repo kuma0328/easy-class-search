7d6d590e176ab2bc90db6ed4ad77c7568c16e141
kuma033 <nhayato28@yahoo.co.jp>
2023-09-18 01:21:58 +0900

    docker setup 完了

---
+++ server/db/initdb.d/1_create_table.sql
@@ -0,0 +1,29 @@
+-- "courses" テーブルの作成
+CREATE TABLE IF NOT EXISTS course (
+  subject_code TEXT NOT NULL PRIMARY KEY,
+  department TEXT NOT NULL
+);
+
+-- "courses-info" テーブルの作成
+CREATE TABLE IF NOT EXISTS course_info (
+  course_id UUID NOT NULL PRIMARY KEY,
+  people INT,
+  rate_a DOUBLE PRECISION,
+  rate_b DOUBLE PRECISION,
+  rate_c DOUBLE PRECISION,
+  rate_d DOUBLE PRECISION,
+  rate_f DOUBLE PRECISION,
+  rate_average DOUBLE PRECISION
+);
+
+-- "courses-list" テーブルの作成
+CREATE TABLE IF NOT EXISTS course_list (
+  course_id UUID NOT NULL PRIMARY KEY,
+  subject_code TEXT NOT NULL,
+  year INT,
+  season TEXT,
+  teacher TEXT,
+  course_url TEXT,
+  FOREIGN KEY (subject_code) REFERENCES course(subject_code),
+  FOREIGN KEY (course_id) REFERENCES course_info(course_id)
+);
