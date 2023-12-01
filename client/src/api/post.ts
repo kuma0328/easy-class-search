427fd41178d8094e4ebac4cca6465acad77a675c
kuma033 <nhayato28@yahoo.co.jp>
2023-12-01 22:36:21 +0900

    add env file

--- client/src/api/post.ts
+++ client/src/api/post.ts
@@ -13,74 +13,85 @@ import TTeacherParam from "src/types/TeacherParam";
 export const getCourseInfoPosts = async (
   params: TFavoriteCourseParam
 ): Promise<TCourseInfo[]> => {
+  const serverURL = process.env.REACT_APP_SERVER_URL;
   const queryString = Object.entries(params)
     .map(([key, value]) => `${key}=${value}`)
     .join("&");
-  const result = await axios.get(`/course_info?${queryString}`);
+  const result = await axios.get(`${serverURL}/course_info?${queryString}`);
   return result.data;
 };
 
 export const getCourseCountPosts = async (
   params: TFavoriteCourseParam
 ): Promise<TCourseCount> => {
+  const serverURL = process.env.REACT_APP_SERVER_URL;
   const queryString = Object.entries(params)
     .map(([key, value]) => `${key}=${value}`)
     .join("&");
-  const result = await axios.get(`/course_count?${queryString}`);
+  const result = await axios.get(`${serverURL}/course_count?${queryString}`);
   return result.data;
 };
 
 export const getSyllabusPosts = async (
   code: string | undefined
 ): Promise<TSyllabus[]> => {
-  const result = await axios.get(`/syllabus?code=${code}`);
+  const serverURL = process.env.REACT_APP_SERVER_URL;
+  const result = await axios.get(`${serverURL}/syllabus?code=${code}`);
   return result.data;
 };
 
 export const getCourseDetailPosts = async (
   code: string | undefined
 ): Promise<TCourseDetail> => {
-  const result = await axios.get(`/course_detail?code=${code}`);
+  const serverURL = process.env.REACT_APP_SERVER_URL;
+  const result = await axios.get(`${serverURL}/course_detail?code=${code}`);
   return result.data;
 };
 
 export const getTeacherGradesPosts = async (
   params: TTeacherParam
 ): Promise<TRadarChart[]> => {
+  const serverURL = process.env.REACT_APP_SERVER_URL;
   const queryString = Object.entries(params)
     .map(([key, value]) => `${key}=${value}`)
     .join("&");
-  const result = await axios.get(`/teacher?${queryString}`);
+  const result = await axios.get(`${serverURL}/teacher?${queryString}`);
   return result.data;
 };
 
 export const getTeacherCountPosts = async (
   params: TTeacherParam
 ): Promise<TTeacherCount> => {
+  const serverURL = process.env.REACT_APP_SERVER_URL;
   const queryString = Object.entries(params)
     .map(([key, value]) => `${key}=${value}`)
     .join("&");
-  const result = await axios.get(`/teacher_count?${queryString}`);
+  const result = await axios.get(`${serverURL}/teacher_count?${queryString}`);
   return result.data;
 };
 
 export const getCourseInfoByTeacherPosts = async (
   teacher: string | undefined
 ): Promise<TCourseInfo[]> => {
-  const result = await axios.get(`/teacher_courses?teacher=${teacher}`);
+  const serverURL = process.env.REACT_APP_SERVER_URL;
+  const result = await axios.get(
+    `${serverURL}/teacher_courses?teacher=${teacher}`
+  );
   return result.data;
 };
 
 export const getCommentByCodePosts = async (
   code: string | undefined
 ): Promise<TComment[]> => {
-  const result = await axios.get(`/comment?code=${code}`);
+  const serverURL = process.env.REACT_APP_SERVER_URL;
+  const result = await axios.get(`${serverURL}/comment?code=${code}`);
   return result.data;
 };
 
 export const addCommentByCodePostts = async (
   params: TCommentParam
 ): Promise<TComment> => {
-  const result = await axios.post(`/comment`, params);
+  const serverURL = process.env.REACT_APP_SERVER_URL;
+  const result = await axios.post(`${serverURL}/comment`, params);
   return result.data;
 };
