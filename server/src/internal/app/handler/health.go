c834c82f35ed3518074e09f0164723646ec82874
kuma033 <nhayato28@yahoo.co.jp>
2023-12-16 17:29:15 +0900

    change /health

--- server/src/internal/app/handler/health.go
+++ server/src/internal/app/handler/health.go
@@ -10,9 +10,15 @@ import (
 func HealthHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Received Request for %s %s", r.Method, r.URL.Path)
 	w.Header().Set("X-Content-Type-Options", "nosniff")
-	if r.Method != http.MethodGet {
+
+	// HEADリクエストも許可する
+	if r.Method != http.MethodGet && r.Method != http.MethodHead {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	io.WriteString(w, "health OK\n")
+
+	// 実際のコンテンツはGETリクエストと同様に返す
+	if r.Method == http.MethodGet {
+		io.WriteString(w, "health OK\n")
+	}
 }
