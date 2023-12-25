e22d7765a79bd780184825ca6546a1c803d0cce8
kuma033 <nhayato28@yahoo.co.jp>
2023-12-25 18:47:59 +0900

    usecase/comment test書いた

--- server/src/internal/app/usecase/comment.go
+++ server/src/internal/app/usecase/comment.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+	"errors"
 
 	"github.com/kuma0328/easy-class-search/internal/app/domain"
 )
@@ -17,6 +18,14 @@ func NewCommentUsecase(cr domain.CommentRepository) domain.CommentUsecase {
 }
 
 func (cu *commentUsecase) AddCommentOfCode(ctx context.Context, code string, comment string) (domain.Comment, error) {
+	if comment == "" {
+		return domain.Comment{}, errors.New("Comment can not be empty")
+	}
+
+	if code == "" {
+		return domain.Comment{}, errors.New("course_code can not be empty")
+	}
+
 	c, err := cu.commentRepo.AddCommentOfCode(ctx, code, comment)
 	return c, err
 }
