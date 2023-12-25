8f85ab5ee9a8478fa1af98224d371283034ddb5d
kuma033 <nhayato28@yahoo.co.jp>
2023-12-25 19:24:31 +0900

    endpoint /syllabus 消した

--- server/src/internal/platform/web/router.go
+++ server/src/internal/platform/web/router.go
@@ -39,7 +39,6 @@ func withDBHandler(handler func(http.ResponseWriter, *http.Request, *sql.DB), db
 func newRouter(db *sql.DB) {
 	http.HandleFunc("/health", handler.HealthHandler)
 	http.HandleFunc("/course_info", handler.GetCourseInfoListWithFliter(db))
-	http.HandleFunc("/syllabus", handler.GetSyllabusOfCode(db))
 	http.HandleFunc("/course_detail", handler.GetCourseDetailOfCode(db))
 	http.HandleFunc("/teacher", handler.GetTeacherAll(db))
 	http.HandleFunc("/teacher_courses", handler.GetCoursesByTeacher(db))
