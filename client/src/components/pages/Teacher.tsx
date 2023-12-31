d17b833039615d0e31d5488f3c6b573c3366b590
kuma033 <nhayato28@yahoo.co.jp>
2023-12-31 09:44:17 +0900

    /teacherのページが戻った時にloadingがかかるのを修正

--- client/src/components/pages/Teacher.tsx
+++ client/src/components/pages/Teacher.tsx
@@ -3,7 +3,6 @@ import initialTeacherParam from "src/static/data/teacherParam";
 import TRadarChart from "src/types/RadarChart";
 import TTeacherCount from "src/types/TeacherCount";
 import TTeacherParam from "src/types/TeacherParam";
-import { v4 as uuidv4 } from "uuid";
 import TeacherColmuns from "../organisms/TeacherColmuns";
 import Loading from "../atoms/Loading";
 import NoData from "../atoms/NoData";
@@ -11,36 +10,15 @@ import MoreInfo from "../molecules/MoreInfo";
 import TeacherList from "../organisms/TeacherList";
 import Footer from "../molecules/Footer";
 import TitleBar from "../organisms/TitleBar";
-import {
-  getTeacherCount,
-  getTeacherGrades,
-  getTeacherParam,
-} from "src/api/get";
+import { getTeacherCount, getTeacherGrades } from "src/api/get";
 import { postTeacherParamById } from "src/api/post";
 export const Teacher = () => {
-  // Cookie から情報を読み取る関数
-  const readFromCookie = (key: string): string | null => {
-    try {
-      document.cookie = "SameSite=None; Secure";
-      const cookies = decodeURIComponent(document.cookie).split(";");
-      for (const cookie of cookies) {
-        const [cookieKey, cookieValue] = cookie.trim().split("=");
-        if (cookieKey === key) {
-          return cookieValue;
-        }
-      }
-      return null;
-    } catch (error) {
-      return null;
-    }
-  };
   const [teacherList, setTeacherList] = useState<TRadarChart[]>([]);
   const [teacherParam, setTeacherParam] = useState<TTeacherParam>(
     () => initialTeacherParam
   );
   const [teacherCount, setTeacherCount] = useState<TTeacherCount>();
-  const [teacherLoading, setTeacherLoading] = useState(true);
-  const [, setUserID] = useState<string>("");
+  const [teacherLoading, setTeacherLoading] = useState(false);
 
   const teacherParamOfMajor = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setTeacherParam((prevParam) => ({
@@ -48,6 +26,7 @@ export const Teacher = () => {
       major: event.target.value,
       teacherOffset: 0,
     }));
+    setTeacherLoading(true);
   };
   const handleTeacherBackClick = () => {
     window.scrollTo({
@@ -57,6 +36,7 @@ export const Teacher = () => {
       ...prevParam,
       teacherOffset: prevParam.teacherOffset - 30,
     }));
+    setTeacherLoading(true);
   };
 
   const handleTeacherNextClick = () => {
@@ -67,44 +47,69 @@ export const Teacher = () => {
       ...prevParam,
       teacherOffset: prevParam.teacherOffset + 30,
     }));
+    setTeacherLoading(true);
   };
 
   useEffect(() => {
-    const userId = readFromCookie("user");
-    if (userId) {
-      setUserID(userId);
-      const fetchData = async () => {
-        const saveTeacherParam = await getTeacherParam(userId);
-        if (saveTeacherParam.id === userId) setTeacherParam(saveTeacherParam);
-        else setTeacherParam({ ...initialTeacherParam, id: userId });
-      };
-      fetchData();
-    } else {
-      const id = uuidv4();
-      setUserID(id);
-      setTeacherParam({ ...initialTeacherParam, id: id });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const fetchData = async () => {
+      const cacheTeacherParam = localStorage.getItem("teacherParam");
+      if (cacheTeacherParam) {
+        setTeacherParam(JSON.parse(cacheTeacherParam));
+      } else {
+        setTeacherParam(initialTeacherParam);
+        localStorage.setItem(
+          "teacherParam",
+          JSON.stringify(initialTeacherParam)
+        );
+      }
+
+      const cacheTeacherInfo = localStorage.getItem("teacherInfo");
+      if (cacheTeacherInfo) {
+        setTeacherList(JSON.parse(cacheTeacherInfo));
+      } else {
+        setTeacherLoading(true);
+        const teacherInfo = await getTeacherGrades(initialTeacherParam);
+        setTeacherList(teacherInfo);
+        localStorage.setItem("teacherInfo", JSON.stringify(teacherInfo));
+        setTeacherLoading(false);
+      }
+
+      const cacheTeacherCount = localStorage.getItem("teacherCount");
+      if (cacheTeacherCount) {
+        setTeacherCount(JSON.parse(cacheTeacherCount));
+      } else {
+        setTeacherLoading(true);
+        const teacherCount = await getTeacherCount(initialTeacherParam);
+        setTeacherCount(teacherCount);
+        localStorage.setItem("teacherCount", JSON.stringify(teacherCount));
+        setTeacherLoading(false);
+      }
+    };
+    fetchData();
   }, []);
 
   useEffect(() => {
-    if (teacherParam.id === "") return;
-    postTeacherParamById(teacherParam);
     const fetchData = async () => {
       try {
         setTeacherLoading(true);
         const teacherCount = await getTeacherCount(teacherParam);
         setTeacherCount(teacherCount);
+        localStorage.setItem("teacherCount", JSON.stringify(teacherCount));
         const teacherInfo = await getTeacherGrades(teacherParam);
         setTeacherList(teacherInfo);
+        localStorage.setItem("teacherInfo", JSON.stringify(teacherInfo));
       } catch (error) {
         console.log(error);
       } finally {
         setTeacherLoading(false);
       }
     };
-    fetchData();
-  }, [teacherParam]);
+    if (teacherLoading) {
+      localStorage.setItem("teacherParam", JSON.stringify(teacherParam));
+      fetchData();
+    }
+  }, [teacherLoading, teacherParam]);
+
   return (
     <div className="w-screen h-screen">
       <TitleBar activeTab="先生検索" />
